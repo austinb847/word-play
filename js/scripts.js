2d23da50f325486b697fc113d8de56021d6a8d95
austinb847 <austinb847@icloud.com>
2020-03-30 20:45:26 -0700

    reverse filtered three word list, then rejoin the words back into a sentence to then display on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
   $("#sentenceForm").submit(function(event) {
     var userSentece = $("#userSentence").val();
-    
+    var words = userSentece.split(' ');
     
 
+    var threeCharWords = words.filter(function(word) {
+      return word.length >= 3;
+    });
+
+    var reverseWordsList = threeCharWords.reverse();
+    var newSentence = reverseWordsList.join().replace(",", " ");
+    
+    $("#sentenceForm").hide();
+    $(".output").text(newSentence);
     
     event.preventDefault();
   });
